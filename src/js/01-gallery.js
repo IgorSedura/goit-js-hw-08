8170cd57b23c1584613b5a053c719fa222c140d4
Igor Sedura <igorsedura@gmail.com>
2022-09-27 20:52:37 +0300

    Video playback

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryCards = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+galleryCards.insertAdjacentHTML('beforeend', galleryMarkup);
+function createGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+
+      `;
+    })
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load } from './storage';
 
+const videoEl = document.querySelector('#vimeo-player');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const player = new Player(videoEl);
+
+function onPlayData(seconds) {
+  save(LOCALSTORAGE_KEY, seconds);
+}
+
+let initPage = load(LOCALSTORAGE_KEY);
+player.on('timeupdate', throttle(onPlayData, 1000));
+
+if (initPage) {
+  const { seconds } = initPage;
+  player.setCurrentTime(seconds);
+}
